c71fc5bab458feaf53adae1031caa05e658ba4d9
Sumbul Kaynat <sumbulkaynat@gmail.com>
2021-01-28 02:00:02 +0530

    Deleting products in firebase

--- src/App.js
+++ src/App.js
@@ -122,11 +122,23 @@ class App extends React.Component {
     handleDeleteProduct = (id) => {
         const {products} = this.state;
 
-        const items = products.filter((item) => item.id !== id);
+        // const items = products.filter((item) => item.id !== id);
 
-        this.setState({
-            products : items
-        })
+        // this.setState({
+        //     products : items
+        // })
+
+        const docRef = this.db.collection('products').doc(id);
+        
+        docRef
+            .delete()
+            .then(() => {
+                console.log("Deleted successfully")
+            })
+            .catch((error) => {
+                console.log("Error : " , error);
+            })
+
     }
 
     getCartCount = () => {
